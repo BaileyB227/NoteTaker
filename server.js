a85296216413fd015b9b25a5db6b79818eb8aceb
BaileyB227 <87950291+BaileyB227@users.noreply.github.com>
2021-09-02 13:24:26 -0500

    added consts and routes

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
-var app = express();
+const fs = require('fs')
+const path = require('path')
+const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`);
+});
