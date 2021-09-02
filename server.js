56aafbf549848abd787013f55a0445617cb24a8d
BaileyB227 <87950291+BaileyB227@users.noreply.github.com>
2021-09-02 13:27:32 -0500

    landing pages

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const fs = require('fs')
 const path = require('path')
 const app = express();
+const fs = require('fs')
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
