9778b73793b914fa5a919ad88962637969aad294
BaileyB227 <87950291+BaileyB227@users.noreply.github.com>
2021-09-02 13:33:43 -0500

    fix

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
 });
